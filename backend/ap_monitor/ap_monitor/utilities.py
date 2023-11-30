245f4024a3418d966a4f410451384988e9feb792
Keegan White <keeganthomaswhite@gmail.com>
2023-11-30 14:15:14 +0200

    added test to utilities file

--- backend/ap_monitor/ap_monitor/utilities.py
+++ backend/ap_monitor/ap_monitor/utilities.py
@@ -13,6 +13,22 @@ def update_prometheus_targets(ip_addresses):
             updated_targets = current_targets.union(ip_addresses)
             job['static_configs'][0]['targets'] = list(updated_targets)
             break
-
+    # filename = 'prometheus_test.yml'
     with open(filename, 'w') as file:
         yaml.dump(data, file, sort_keys=False)
+
+def validate_yaml(filename):
+    try:
+        with open(filename, 'r') as file:
+            yaml.safe_load(file)
+        return True
+    except yaml.YAMLError as exc:
+        print(f"Error in configuration file: {exc}")
+        return False
+if __name__ == '__main__':
+    update_prometheus_targets(['8.8.8.8', '10.0.0.1', '192.168.1.1'])
+    is_valid = validate_yaml('prometheus_test.yml')
+    if is_valid:
+        print("YAML file is valid")
+    else:
+        print("YAML file is invalid")
