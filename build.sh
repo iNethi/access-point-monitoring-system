31ea754b52f626346d736fe5f5689ab0c03b00cc
Keegan White <keeganthomaswhite@gmail.com>
2023-12-11 14:17:34 +0200

    updated,compose

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 cd backend/ap_monitor || exit
 docker-compose up -d inethi-ap-monitor-mysql || exit
-#sleep 20 || exit
-#docker-compose build --no-cache && docker-compose up -d || exit
+sleep 20 || exit
+docker-compose build --no-cache && docker-compose up -d || exit
 cd ../.. || exit
 
 cd ui/ap-monitor || exit
