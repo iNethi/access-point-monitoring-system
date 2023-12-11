61d881d316ed1943a42a43ad101a77772da70061
Keegan White <keeganthomaswhite@gmail.com>
2023-12-11 13:49:56 +0200

    updated requirements.txt and dockerfile

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 cd backend/ap_monitor || exit
 docker-compose up -d inethi-ap-monitor-mysql || exit
+sleep 20 || exit
 docker-compose build --no-cache && docker-compose up -d || exit
 cd ../.. || exit
 
