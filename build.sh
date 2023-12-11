117ef9efb61e1804154ffebedba74eb584be6605
Keegan White <keeganthomaswhite@gmail.com>
2023-12-11 14:04:18 +0200

    updated requirements.txt and dockerfile

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 cd backend/ap_monitor || exit
 docker-compose up -d inethi-ap-monitor-mysql || exit
-sleep 20 || exit
-docker-compose build --no-cache && docker-compose up -d || exit
+#sleep 20 || exit
+#docker-compose build --no-cache && docker-compose up -d || exit
 cd ../.. || exit
 
-cd ui || exit
+cd ui/ap-monitor || exit
 docker-compose up -d
 
 echo DONE!
