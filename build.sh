7a7eec46674e550991d15a7308c2f6de9e124a44
Keegan White <keeganthomaswhite@gmail.com>
2023-12-20 09:59:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd backend/ap_monitor || exit
-docker-compose up -d inethi-ap-monitor-mysql || exit
+docker compose up -d inethi-ap-monitor-mysql || exit
 sleep 20 || exit
-docker-compose build --no-cache && docker-compose up -d || exit
+docker compose build --no-cache && docker compose up -d || exit
 cd ../.. || exit
 
 cd ui/ap-monitor || exit
-docker-compose up -d
+docker compose up -d
 
 echo DONE!
